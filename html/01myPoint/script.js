5d8b8bc214b83a9ed3f1a7a0c82e1196c7b029d0
26689 <tasso.mevissen@gmail.com>
2019-09-09 11:41:17 +0200

    Random spawnpoint

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -6,3 +6,20 @@ const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
+
+function animate()
+{
+  requestAnimationFrame(animate);
+  let color = "rgb(" + getRandom(255) +"," + getRandom(255) + "," + getRandom(255) + ")"
+  let A = new Point(new Vector2d(getRandom(width),getRandom(height)),100,color);
+  A.draw(context)
+
+}
+
+animate()
+
+function getRandom(max)
+{
+  let ans = Math.floor(Math.random()*max);
+  return ans;
+}
