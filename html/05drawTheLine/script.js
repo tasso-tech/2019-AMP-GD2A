62a81dc30a15e228e9edc73b581756810e0a850c
26689 <tasso.mevissen@gmail.com>
2019-09-27 16:02:26 +0200

    Updated script

--- html/05drawTheLine/script.js
+++ html/05drawTheLine/script.js
@@ -9,7 +9,14 @@ canvas.height = height;
 
 let f = new LinearFunction(-0.2,200);
 
+
+
+let A = new Point(new Vector2d(200,200),10,"blue");
+let B = new Point(new Vector2d(500,300),10,"red");
+
+A.draw(context); B.draw(context);
+
 for(let x = 0; x < width ; x+= 10){
-  let point = new Point(new Vector2d(x, f.y(x)),10,"yellow");
+  let point = new Point(new Vector2d(x, f.y(x)),5,"yellow");
   point.draw(context);
 }
