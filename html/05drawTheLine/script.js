c4abf273435423cc89d275a385ac4e9d8b33403e
26689 <tasso.mevissen@gmail.com>
2019-09-27 16:01:26 +0200

    05 added script,index and lib

--- html/05drawTheLine/script.js
+++ html/05drawTheLine/script.js
@@ -6,3 +6,10 @@ const height = window.innerHeight;
 
 canvas.width = width;
 canvas.height = height;
+
+let f = new LinearFunction(-0.2,200);
+
+for(let x = 0; x < width ; x+= 10){
+  let point = new Point(new Vector2d(x, f.y(x)),10,"yellow");
+  point.draw(context);
+}
